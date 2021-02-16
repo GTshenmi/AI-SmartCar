1e179c2fb265cdc46c9e9b08a1549045f8f34b30
GTshenmi <936305695@qq.com>
2021-02-16 12:58:39 +0800

    v1.0.3
    None.

--- TC264Project/SmartCar/LQ_TC264DA_LIB_ADS/App/Sys/pid_ctrl.c
+++ TC264Project/SmartCar/LQ_TC264DA_LIB_ADS/App/Sys/pid_ctrl.c
@@ -5,69 +5,69 @@
 void PID_CtrlDemo()
 {
 #if 1
-	 PID_TypeDef PID_CtrlStr = PID_Init(PositionalPID);
-	
-	 PID_SetValue(&PID_CtrlStr,PIDValue(2.0,0.0,1.0));
-	
-	 PID_SetGain(&PID_CtrlStr,PIDGainValue(10.0,10.0));
+    PID_TypeDef PID_CtrlStr = PID_Init(PositionalPID);
+
+    PID_SetValue(&PID_CtrlStr,PIDValue(2.0,0.0,1.0));
+
+    PID_SetGain(&PID_CtrlStr,PIDGainValue(10.0,10.0));
 #else 
-	 PID_TypeDef PID_CtrlStr = PID_Init(PositionalPID); 
-	
-	 PID_CtrlStr.Kp = 2.0;
-	 PID_CtrlStr.Ki = 0.0;
-	 PID_CtrlStr.Kd = 1.0;
-	
-	 PID_CtrlStr.Gain.Input = 10.0;
-	 PID_CtrlStr.Gain.OutPut = 10.0;
+    PID_TypeDef PID_CtrlStr = PID_Init(PositionalPID);
+
+    PID_CtrlStr.Kp = 2.0;
+    PID_CtrlStr.Ki = 0.0;
+    PID_CtrlStr.Kd = 1.0;
+
+    PID_CtrlStr.Gain.Input = 10.0;
+    PID_CtrlStr.Gain.OutPut = 10.0;
 #endif
-	 float target_value = 100.0;
-	 float actual_value = 0.0;
-	 while(1)
-	 {
-                
-		PID_Ctrl(&PID_CtrlStr,target_value,actual_value);
-                /*actual_value = get_actual_value();*/
-	 }
+    float target_value = 100.0;
+    float actual_value = 0.0;
+    while(1)
+    {
+
+       PID_Ctrl(&PID_CtrlStr,target_value,actual_value);
+               /*actual_value = get_actual_value();*/
+    }
 }
 
 PID_TypeDef PID_Init(pid_type_t PID_Type)
 {
-	 PID_TypeDef PID_CtrlStr = 
-	 {
-			 .Kp = 0.0,
-			 .Ki = 0.0,
-			 .Kd = 0.0,
-			
-			 .PID_Error = {0.0,0.0,0.0},
-			 .IntegralValue = 0.0,
-			
-			 .Result = 0.0,
-			
-			 .Gain.Input = 1.0,
-			 .Gain.Output = 1.0,
-			 .MaxIntegralValue = 100.0,
-			
-			 .PID_Type = PositionalPID,
-	 };
-	 
-	 PID_CtrlStr.PID_Type = PID_Type;
-	 
-	 return PID_CtrlStr;
+    PID_TypeDef PID_CtrlStr =
+    {
+            .Kp = 0.0,
+            .Ki = 0.0,
+            .Kd = 0.0,
+
+            .PID_Error = {0.0,0.0,0.0},
+            .IntegralValue = 0.0,
+
+            .Result = 0.0,
+
+            .Gain.Input = 1.0,
+            .Gain.Output = 1.0,
+            .MaxIntegralValue = 100.0,
+
+            .PID_Type = PositionalPID,
+    };
+
+    PID_CtrlStr.PID_Type = PID_Type;
+
+    return PID_CtrlStr;
 }
 
 PID_TypeDef PID_SetValue(PID_TypeDef *PID_CtrlStr,pid_ctrl_t PIDValue)
 {
-	 PID_CtrlStr->Kp = PIDValue.Kp;
-	 PID_CtrlStr->Ki = PIDValue.Ki;
-	 PID_CtrlStr->Kd = PIDValue.Kd;
-	
-	 return *PID_CtrlStr;
+    PID_CtrlStr->Kp = PIDValue.Kp;
+    PID_CtrlStr->Ki = PIDValue.Ki;
+    PID_CtrlStr->Kd = PIDValue.Kd;
+
+    return *PID_CtrlStr;
 }
 
 PID_TypeDef PID_SetGain(PID_TypeDef *PID_CtrlStr,pid_gain_t GainValue)
 {
-	 PID_CtrlStr->Gain = GainValue;
-	 return *PID_CtrlStr;
+    PID_CtrlStr->Gain = GainValue;
+    return *PID_CtrlStr;
 }
 
 PID_TypeDef *PID_Ctrl(PID_TypeDef *PID_CtrlStr,float TargetValue,float ActualValue)
@@ -113,21 +113,21 @@ PID_TypeDef *PID_Ctrl(PID_TypeDef *PID_CtrlStr,float TargetValue,float ActualVal
 
 pid_ctrl_t PIDValue(float Kp,float Ki,float Kd)
 {
-	 pid_ctrl_t PIDValue = {0,0,0};
-	 
-	 PIDValue.Kp = Kp;
-	 PIDValue.Ki = Ki;
-	 PIDValue.Kd = Kd;
-	 
-	 return PIDValue;
+    pid_ctrl_t PIDValue = {0,0,0};
+
+    PIDValue.Kp = Kp;
+    PIDValue.Ki = Ki;
+    PIDValue.Kd = Kd;
+
+    return PIDValue;
 }
 
 pid_gain_t PIDGainValue(float inputgain,float outputgain)
 {
-	 pid_gain_t GainValue = {0,0};
-	 
-	 GainValue.Input = inputgain;
-	 GainValue.Output = outputgain;
-	 
-	 return GainValue;
+    pid_gain_t GainValue = {0,0};
+
+    GainValue.Input = inputgain;
+    GainValue.Output = outputgain;
+
+    return GainValue;
 }
